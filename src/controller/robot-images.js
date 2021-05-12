61d5ab2b76043ca46966b8453792186c9497be8c
Romulo F <romulo-gouvea@hotmail.com>
2021-05-12 19:27:21 -0300

    finalizando os ajustes das imagens e a geração delas

--- src/controller/robot-images.js
+++ src/controller/robot-images.js
@@ -7,64 +7,57 @@ const font2base64 = require('node-font2base64');
 import { constants } from "../../config";
 import { UArchive, UString } from "~/utils";
 
+const font_title = font2base64.encodeToDataUrlSync(`${constants.ASSETS_FOLDER}/fonts/poppins/Poppins-Bold.ttf`);
+const font_content = font2base64.encodeToDataUrlSync(`${constants.ASSETS_FOLDER}/fonts/poppins/Poppins-Regular.ttf`);
+
+let image = UArchive.readFile(`${constants.ASSETS_FOLDER}/images`, 'felipe.png');
+const base64Image = new Buffer.from(image).toString('base64');
+const image_felipe = 'data:image/jpeg;base64,' + base64Image
+
+const today = new Date();
+const pathFolderImages = `./src/assets/output/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
+
 const RobotImages = async () => {
   console.log("Iniciando a geração das imagens");
 
   //Filtrando os textos
   let textScraped = extractTextOfFileScrapy();
-  console.log(textScraped.length);
+  console.log(textScraped.length + " notícias");
 
-  // var title = "Gangue de ransomware DarkSide anuncia que realizará análise preliminar antes de permitir novos ataques:"
-  // var content = `A decisão veio após um de seus “parceiros” invadir e bloquear o sistema de oleodutos que
-  // fornece 45% de todo o combustível utilizado na costa leste dos EUA, obrigando o governo a decretar estado de
-  // emergência. Em um comunicado, o grupo criminoso se declarou apolítico, não possuindo ligação com nenhum
-  // governo. Seu objetivo primário seria apenas ganhar dinheiro com a cobrança de resgates e quer evitar
-  // consequências sociais de seus ataques no futuro. As informações são do site Bleeping Computer.
-  // A decisão veio após um de seus “parceiros” invadir e bloquear o sistema de oleodutos que`;
-
-  let today = new Date();
-  let pathFolder = `./src/assets/output/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
-
-
-  //Generate images Stories
   for (let [index, news] of textScraped.entries()) {
 
-    let titleName = news.title.toLowerCase().replace(/\s/g, '_').substring(0, 10);
-    let nameFile = `${today.getDate()}_${titleName}-img.png`;
-    let output = UString.removeAcento(pathFolder + "/stories/" + nameFile);
-
-    UArchive.createFolder(output);
-
-    let generated = await generateImage(
+    await generateImage(
+      'STORIES',
       news.title,
       news.content,
       "Cadastre na newsletter e receba todas as notícias",
-      output
     );
 
-    console.log(`${index} - ${generated} - ${news.title}`);
+    await generateImage(
+      'FEED',
+      news.title,
+      news.content,
+      "Cadastre na newsletter e receba todas as notícias",
+    );
+
+    console.log(`Gerada - ${index + 1} - ${news.title}`);
   }
 
   console.log("Geração das imagens finalizada");
   process.exit();
 };
 
-function generateImage(title, content, action, output = './image1.png') {
+function generateImage(type, title, content, action, output = './image1.png') {
 
   content = UString.capitalizeFirstLetter(content);
 
   if (content.length > 500) {
-    content = content.substring(0, 500) + " [Veja mais na newsletter]"
+    content = content.substring(0, 500) + "... [Veja mais na newsletter]"
   }
 
-  const font_title = font2base64.encodeToDataUrlSync(`${constants.ASSETS_FOLDER}/fonts/poppins/Poppins-Bold.ttf`);
-  const font_content = font2base64.encodeToDataUrlSync(`${constants.ASSETS_FOLDER}/fonts/poppins/Poppins-Regular.ttf`);
-
-  let image_felipe = UArchive.readFile(`${constants.ASSETS_FOLDER}/images`, 'felipe.png');
-  const base64Image = new Buffer.from(image_felipe).toString('base64');
-  image_felipe = 'data:image/jpeg;base64,' + base64Image
-
-  var htmlFinish = `
+  let htmlFinish = ``;
+  if (type == 'STORIES') {
+    htmlFinish = `
     <html lang="pt">
 
     <head>
@@ -164,6 +157,117 @@ function generateImage(title, content, action, output = './image1.png') {
 
     </html>
   `;
+  } else {
+    htmlFinish = `
+    <html lang="pt">
+
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Feeds</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <style>
+            @font-face {
+              font-family: 'PoppinsBold';
+              src: url(${font_title}) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'PoppinsRegular';
+                src: url(${font_content}) format('truetype');
+            }
+
+            body {
+                /* background-color: black; */
+                width: 1080px;
+                min-height: 1080px;
+                max-height: 1080px;
+                font-family: 'PoppinsRegular';
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                margin: 0;
+                padding: 0;
+            }
+
+            div {
+                padding: 50px;
+                background-color: #FFF;
+            }
+
+            .div1 {
+                flex: 0.75;
+                justify-content: flex-start;
+                padding-bottom: 30px;
+                max-height: 810px;
+                min-height: 810px;
+            }
+
+            .div2 {
+                flex: 0.25;
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+                background-color: rgb(242, 218, 94);
+                max-height: 270px;
+                min-height: 270px;
+            }
+
+            p {
+                font-size: 35px;
+                margin: 0;
+                padding: 0;
+            }
+
+            .bold {
+              font-family: 'PoppinsBold';
+            }
+            
+            .title {
+                font-size: 40px;
+            }
+
+            .content {
+                margin-top: 20px;
+            }
+
+            .action {
+                margin-top: 20px;
+            }
+
+            img {
+                border-radius: 51%;
+                border: 30px solid #FFF;
+                margin: 0;
+                padding: 0;
+                min-width: 130px;
+                height: 130px;
+                margin-top: -160px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="div1">
+            <p class="title bold">{{title}}</p>
+            <p class="content">{{content}}</p>
+        </div>
+        <div class="div2">
+            <img src="{{image_felipe}}" />
+            <p class="action bold">{{action}}</p>
+        </div>
+    </body>
+
+    </html>
+  `;
+  }
+
+  let titleName = title.toLowerCase().replace(/\s/g, '_').substring(0, 10);
+  let nameFile = `${today.getDate()}_${titleName}_${type.toLowerCase()}.png`;
+  output = UString.removeAcento(`${pathFolderImages}/${type.toLowerCase()}/${nameFile}`);
+
+  UArchive.createFolder(output);
 
   return new Promise((resolve, reject) => {
     nodeHtmlToImage({
